b8312f6ae3a89836e11e61886c6ac89f8fc88915
AlexandrAAkulina <icomfy1484@gmail.com>
2023-10-11 16:44:15 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+import '../css/common.css';
+
+const startButtonEl = document.querySelector('[data-start]');
+const stoptButtonEl = document.querySelector('[data-stop]');
+
+let counter = null;
+
+startButtonEl.addEventListener('click', onStartButtonClick);
+stoptButtonEl.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick () {
+    startButtonEl.disable = true;
+    stoptButtonEl.disable = false;
+    counter = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopButtonClick() {
+    stoptButtonEl.disable = true;
+    startButtonEl.disable = false;
+    clearInterval(counter);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
+
 
