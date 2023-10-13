17520e36f183f99a1b5f1fe49fdbc68e17ce1381
AlexandrAAkulina <icomfy1484@gmail.com>
2023-10-13 14:29:56 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,18 @@
+import Notiflix from 'notiflix';
+
+const selectors = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name=delay]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]')
+}
+
+selectors.form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
+  const promise = new Promise((res, rej) => {
+    setTimeout()
+  })
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
